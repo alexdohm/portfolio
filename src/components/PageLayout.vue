8d3e3603cf2430883ff82b796197b0afa6260fbd
alexdohm <a@isotronic.de>
2023-04-30 21:45:48 +0200

    more styling updates to home page

--- src/components/PageLayout.vue
+++ src/components/PageLayout.vue
@@ -1,24 +1,20 @@
 <template>
-  <q-layout view="hHh lpR fFf">
+  <q-layout view="hHh lpr fFf">
     <q-header elevated class="bg-primary text-white">
       <q-toolbar>
         <q-avatar>
-          <router-link to="/" class="a-custom-link">AD</router-link>
+          <q-btn to="/" label="AD"/>
         </q-avatar>
         <q-space/>
-        <router-link to="/portfolio" class="a-custom-link">
-          <q-btn v-if="$q.screen.gt.xs" flat dense no-wrap icon="data_object" no-caps label="Portfolio"
-                 class="q-ml-sm q-px-md"/>
-        </router-link>
-        <router-link to="/contact" class="a-custom-link">
-          <q-btn v-if="$q.screen.gt.xs" flat dense no-wrap icon="o_contact_page" no-caps label="Contact"
-                 class="q-ml-sm q-px-md"/>
-        </router-link>
+        <q-btn v-if="$q.screen.gt.xs" flat dense no-wrap to="/portfolio" icon="data_object" no-caps label="Portfolio"
+               class="q-ml-sm q-px-md"/>
+        <q-btn v-if="$q.screen.gt.xs" flat dense no-wrap to="/contact" icon="o_contact_page" no-caps label="Contact"
+               class="q-ml-sm q-px-md"/>
         <q-btn v-if="$q.screen.xs" dense flat round icon="menu" @click="toggleRightDrawer"/>
       </q-toolbar>
     </q-header>
 
-    <q-drawer v-model="rightDrawerOpen" side="right" overlay show-if-above elevated>
+    <q-drawer v-model="rightDrawerOpen" side="right" behavior="mobile" bordered>
       <div>
         <router-link to="/portfolio" @click="toggleRightDrawer">Portfolio</router-link>
       </div>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,24 +1,26 @@
 <template>
   <div class="row justify-center q-col-gutter-x-md">
-    <div class="col-12 col-md-2">
+    <div class="col-8 col-sm-5 col-md-3 col-xl-2 q-mb-md q-mb-md-none">
       <q-img
           :src="imageSrc"
       />
     </div>
-    <div class="col-12 col-md-6 text-left">
-      <div class="q-mb-sm">
+    <div class="col-12 col-md-6 col-lg-4 col-xl-3 text-left q-gutter-lg">
+      <div>
         Hi, I'm Alex, a Full Stack developer from the USA living in Berlin. I have over 5 years of experience designing,
         building and deploying websites and databases. I have degrees in Computer Science and Mechanical Engineering and
         graduated top in my class at the University of Colorado at Boulder.
       </div>
-      <div class="q-mb-sm">
+      <div>
         I can help you with anything website related, whether it is implementing a design, building a server, deploying
         a site or designing a database.
       </div>
       <div>
-        Feel free to contact me at the email below and we can discuss your project. I actively support LGTBQ+ projects
+        Feel free to contact me at the email below and we can discuss your project. I actively support the LGTBQ+ community
         and projects within the art and music community.
       </div>
+      <q-btn color="primary" to="/contact" label="Contact" />
+      <q-btn color="primary" to="/portfolio" label="View my portfolio" />
     </div>
   </div>
 </template>
